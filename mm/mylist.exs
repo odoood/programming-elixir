b078d85bc32d9418c2be7d0b224a7c98b5e59393
odoood <2523476+odoood@users.noreply.github.com>
2020-07-20 20:24:00 -0400

    Add max function to MyList module

--- mm/mylist.exs
+++ mm/mylist.exs
@@ -3,4 +3,14 @@ defmodule MyList do
   def mapsum([ head | tail ], f) do
     f.(head) + mapsum(tail, f)
   end
+
+  def max([]), do: nil
+  def max([head|tail]) do
+    tm = max(tail)
+    if tm === nil or head > tm do
+      head
+    else
+      tm
+    end
+  end
 end
