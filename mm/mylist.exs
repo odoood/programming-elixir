fa8e8390e0b323bbeb789a83fe5c88de099a075e
odoood <2523476+odoood@users.noreply.github.com>
2020-07-20 21:14:52 -0400

    Add caesar function to MyList module

--- mm/mylist.exs
+++ mm/mylist.exs
@@ -13,4 +13,18 @@ defmodule MyList do
       tm
     end
   end
+
+  # Caesar cipher function - works only with lowercase alpha, cause laziness
+  def caesar([], _), do: []
+  def caesar([head|tail], n) when is_integer(n) and n >= 0
+  do
+    # IO.puts "#{head}, #{rem(head+n-97,26)+97}"
+    if head >= 97 and head <= 97+26 do
+      [97 + rem(head + n - 97, 26) | caesar(tail, n)]
+    else
+      [head | caesar(tail, n)]
+    end
+  end
 end
+
+# MyList.caesar('ryvkve', 13)
