e6bcde448242973f867c18451d8b83868355e4c4
odoood <2523476+odoood@users.noreply.github.com>
2020-08-02 11:50:35 -0400

    ListsAndRecursion-8: add get_totals function

---
+++ lists/liandre-8.exs
@@ -0,0 +1,22 @@
+# ListsAndRecursion-8
+defmodule Liandre8 do
+  def get_totals(rates, orders) when is_list(rates) and is_list(orders) do
+    for [{:id, i}, {:ship_to, state}, {:net_amount, amount}] <- orders do
+      [{:id, i}, {:ship_to, state}, {:net_amount, amount},
+       {:total_amount, (1 + Keyword.get(rates, state, 0)) * amount}]
+    end
+  end
+end
+
+
+tax_rates = [NC: 0.075,TX: 0.08]
+orders = [
+  [id: 123,ship_to: :NC,net_amount: 100.00],
+  [id: 124,ship_to: :OK,net_amount: 35.50],
+  [id: 125,ship_to: :TX,net_amount: 24.00],
+  [id: 126,ship_to: :TX,net_amount: 44.80],
+  [id: 127,ship_to: :NC,net_amount: 25.00],
+  [id: 128,ship_to: :MA,net_amount: 10.00],
+  [id: 129,ship_to: :CA,net_amount: 102.00],
+  [id: 130,ship_to: :NC,net_amount: 50.00]
+]
