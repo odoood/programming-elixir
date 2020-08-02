5fc0d87e6f049ea2adc32912647fe66c75474170
odoood <2523476+odoood@users.noreply.github.com>
2020-08-02 13:26:02 -0400

    StringsAndBinaries-2: add anagram? function

--- strings/strandbin.exs
+++ strings/strandbin.exs
@@ -2,4 +2,14 @@ defmodule StrAndBin do
   def is_char_str(chars) when is_list(chars) do
     Enum.all?(chars, &(&1 >= ?\s and &1 <= ?~))
   end
+
+  # Return true if words are anagrams of each other
+  def anagram?(word1, word2) when is_list(word1) and is_list(word2) do
+    if ! (is_char_str(word1) and is_char_str(word2)) do
+      raise "Both words must be character strings"
+    end
+
+    # They must be the same length and have the same chars
+    Enum.count(word1) == Enum.count(word2) and word1 -- word2 == []
+  end
 end
