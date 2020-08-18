a7751f92989c4713aabf1bb65bae87fe8e641312
odoood <2523476+odoood@users.noreply.github.com>
2020-08-18 19:40:33 -0400

    Add a describe block to the tests

--- tooling/test/describe.exs
+++ tooling/test/describe.exs
@@ -1,19 +1,21 @@
 defmodule TestStats do
   use ExUnit.Case
 
-  test "calculates sum" do
-    list = [1, 3, 5, 7, 9]
-    assert Stats.sum(list) == 25
-  end
+  describe "Stats on lists of ints" do
+    test "calculates sum" do
+      list = [1, 3, 5, 7, 9]
+      assert Stats.sum(list) == 25
+    end
 
-  test "calculates count" do
-    list = [1, 3, 5, 7, 9]
-    assert Stats.count(list) == 5
-  end
+    test "calculates count" do
+      list = [1, 3, 5, 7, 9]
+      assert Stats.count(list) == 5
+    end
 
-  test "calculates average" do
-    list = [1, 3, 5, 7, 9]
-    assert Stats.average(list) == 5
+    test "calculates average" do
+      list = [1, 3, 5, 7, 9]
+      assert Stats.average(list) == 5
+    end
   end
 end
 
