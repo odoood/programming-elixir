1eaddab1bcb4e7898b843df5350fb06416b4eb6e
odoood <2523476+odoood@users.noreply.github.com>
2020-08-18 21:39:52 -0400

    Add 2 property tests

--- tooling/test/stats_property_test.exs
+++ tooling/test/stats_property_test.exs
@@ -3,9 +3,26 @@ defmodule StatsPropertyTest do
   use ExUnitProperties
 
   describe "Stats on lists of ints" do
+
+    property "count not negative" do
+      check all l <- list_of(integer()) do
+        assert Stats.count(l) >= 0
+      end
+    end
+
     property "single element lists are their own sum" do
       check all number <- integer() do
         assert Stats.sum([number]) == number
+      end
+    end
+
+    property "sum equals average times count" do
+      check all l <- list_of(integer()) do
+        assert_in_delta(
+          Stats.sum(l),
+          Stats.count(l) * Stats.average(l),
+          1.0e-6
+        )
       end
     end
   end
