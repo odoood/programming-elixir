55236af8836930d973f4af3f9e4751813ad803a8
odoood <2523476+odoood@users.noreply.github.com>
2020-08-23 15:56:27 -0400

    TickerClient: Add TickerClient.start fn to call register fn

--- nodes/tickerclient.ex
+++ nodes/tickerclient.ex
@@ -2,11 +2,30 @@ defmodule TickerClient do
   @moduledoc """
   Documentation for `TickerClient`.
 
-  The Ticker server & Client modules, consolidated. TickerClient acts as a
+  The `Ticker` server & `Client` modules, consolidated. `TickerClient` acts as a
   a server and client, and all instances are connected in a ring.
 
-  A particular TickerClient acts as a server for the "next" TickerClient and a
-  client for the "previous" TickerClient in the ring (it's both server & client
-  for itself where there is a sole TickerClient).
+  A particular `TickerClient` acts as server for the "next" `TickerClient` and
+  client for the "previous" `TickerClient` in the ring (it's both for itself
+  where there is a sole `TickerClient`).
   """
+
+  @interval 2000    # 2 seconds
+  @name     :og     # The original client
+
+  @doc """
+  start.
+
+  Create a new server/client in the ring, attaching it to the "end" of the ring,
+  just behind the original (if it exists).
+  """
+  def start do
+    # Register the new client in the ring, with the original PID as a reference
+    register(:global.whereis_name @name)
+  end
+
+  defp register(:undefined) do
+    # This is the first client in the ring so create and register its PID
+
+  end
 end
