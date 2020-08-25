821081620b179b31c2a164b39f513f6ae9c4b262
odoood <2523476+odoood@users.noreply.github.com>
2020-08-24 23:35:47 -0400

    StackServer: add external api to wrap GenServer calls

--- otp/stackserver.ex
+++ otp/stackserver.ex
@@ -1,6 +1,23 @@
 defmodule StackServer do
   use GenServer
 
+  @name :stack
+
+  #####
+  # External API
+  ####
+  def start_link(current_stack) do
+    GenServer.start_link(__MODULE__, current_stack, name: @name)
+  end
+
+  def pop do
+    GenServer.call @name, :pop
+  end
+
+  def push(item) do
+    GenServer.cast @name, {:push, item}
+  end
+
   # Simplify the stack by reversing the list order
   def init([]),     do: {:ok, []}
   def init([h|t]),  do: {:ok, Enum.reverse([h|t])}
