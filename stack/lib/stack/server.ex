0b27c67413c0737f828d3e5f7960e6faf54e8e88
odoood <2523476+odoood@users.noreply.github.com>
2020-08-27 00:42:36 -0400

    StackServer: copy Stack.Server module to lib dir

---
+++ stack/lib/stack/server.ex
@@ -0,0 +1,36 @@
+defmodule Stack.Server do
+  use GenServer
+
+  #####
+  # External API
+  ####
+  def start_link(current_stack) do
+    GenServer.start_link(__MODULE__, current_stack, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(item) do
+    GenServer.cast __MODULE__, {:push, item}
+  end
+
+  # Simplify the stack by reversing the list order
+  def init([]),     do: {:ok, []}
+  def init([h|t]),  do: {:ok, Enum.reverse([h|t])}
+
+  # Pop a value from the "end" of the stack
+  def handle_call(:pop, _from, [tip|rest]) do
+    {:reply, tip, rest }
+  end
+
+  def handle_cast({:push, new_item}, current_stack) do
+    {:noreply, [new_item|current_stack]}
+  end
+
+  def terminate(reason, state) do
+    IO.puts "**Server terminating, the reason:\n#{inspect reason, pretty: true}"
+    IO.puts "**Server terminating, the state:\n#{inspect state}"
+  end
+end
