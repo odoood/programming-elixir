b8b872d611391661e4b0878a3b81052abb036eee
odoood <2523476+odoood@users.noreply.github.com>
2020-09-06 20:59:27 -0400

    Add Enumerable.reduce protocol implementation for Midi

--- protocols/midi.exs
+++ protocols/midi.exs
@@ -22,3 +22,34 @@ defmodule Midi do
     %Midi{content: File.read!(name)}
   end
 end
+
+defimpl Enumerable, for: Midi do
+  def _reduce(_content, {:halt, acc}, _fun) do
+    {:halted, acc}
+  end
+
+  def _reduce(content, {:suspend, acc}, fun) do
+    {:suspended, acc, &_reduce(content, &1, fun)}
+  end
+
+  def _reduce(_content = "", {:cont, acc}, _fun) do
+    {:done, acc}
+  end
+
+  def _reduce(<<
+                  type::binary-4,
+                  length::integer-32,
+                  data::binary-size(length),
+                  rest::binary
+              >>,
+              {:cont, acc},
+              fun
+              ) do
+    frame = %Midi.Frame{type: type, length: length, data: data}
+    _reduce(rest, fun.(frame, acc), fun)
+  end
+
+  def reduce(%Midi{content: content}, state, fun) do
+    _reduce(content, state, fun)
+  end
+end
